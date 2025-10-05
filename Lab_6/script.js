b60651271cff84d22d1356c27081d923d7097b0f
Susanne Jimenez <smj376@nau.edu>
2025-10-04 22:37:11 -0700

    Updayed Java response

--- Lab_6/script.js
+++ Lab_6/script.js
@@ -1,5 +1,5 @@
 function sayHello() {
   const name = document.getElementById('nameInput').value;
-  const greeting = `🚀 Welcome aboard, Captain ${name}! Take your protein pill and get your JavaScript helmet on!. 🌌`;
+  const greeting = `🚀 Welcome aboard, Captain ${name}! Take your protein pill and get your JavaScript helmet on! ⚡`;
   document.getElementById('greeting').textContent = greeting;
 }
