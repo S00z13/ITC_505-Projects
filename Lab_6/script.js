247a2fe9836a777b8d6a63ed41655f85f93b387c
S00z13 <smj376@nau.edu>
2025-10-26 18:45:07 -0700

    Delete Lab_6 directory
    Updating for Mid-Term Projects merge.

--- Lab_6/script.js
+++
@@ -1,5 +0,0 @@
-function sayHello() {
-  const name = document.getElementById('nameInput').value;
-  const greeting = `🚀 Welcome aboard, Captain ${name}! Take your protein pill and get your JavaScript helmet on! ⚡`;
-  document.getElementById('greeting').textContent = greeting;
-}
